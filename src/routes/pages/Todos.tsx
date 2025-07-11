c7ce17641ddbad698f7fbf0897b07fe1db39fe85
ParkYoungWoong <thesecon@gmail.com>
2025-07-11 13:52:51 +0900

    07-11 13:52

--- src/routes/pages/Todos.tsx
+++ src/routes/pages/Todos.tsx
@@ -3,9 +3,9 @@ import TodoList from '@/components/todos/TodoList'
 
 export default function Todos() {
   return (
-    <>
+    <main className="mx-auto max-w-[700px] p-5">
       <TodoCreator />
       <TodoList />
-    </>
+    </main>
   )
 }
